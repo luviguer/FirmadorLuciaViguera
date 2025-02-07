75d8a7a6cf0a7eaebb41b89223b5d035c381f1d6
luviguer <luviguera@gmail.com>
2025-02-07 12:30:30 +0100

    primer commit

---
+++ firmador/src/main/java/es/arlabdevelopments/firmador/FirmadorJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package es.arlabdevelopments.firmador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmadorJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmadorJavaSpringApplication.class, args);
+	}
+
+}
